7fc4f9b03d286db30d448574702fd6187f50be90
Murilo dacio <murilodacio@gmail.com>
2021-12-16 10:42:39 -0300

    Add redes sociais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import { Button, HStack, Image, Link, Stack, Text } from '@chakra-ui/react'
+import { Button, HStack, Icon, Image, Link, Stack, Text } from '@chakra-ui/react'
+
+import Head from 'next/head'
+import { RiFacebookLine, RiInstagramLine } from 'react-icons/ri'
 
 export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Nova Nota Fiscal - Secretaria de Fazenda de Itaboraí</title>
+      </Head>
+
       <Stack w="100vw" h="100vh" align="center" justify="center" m="0 auto" maxWidth="1120" spacing={12} p="4">
         <HStack>
           <Image src="/prefeitura.png" w="150px" h="40px" alt="prefeitura"/>
@@ -14,6 +21,14 @@ export default function Home() {
           um e-mail ou falar conosco através do chat de atendimento abaixo, nossos operadores estão prontos para responder você! Se optar por mandar um e-mail, o tempo médio de resposta é de no máximo 16 horas devido a alta demanda.
         </Text>
         <Button href="mailto:contato.fazenda@itaborai.rj.gov.br" as={Link} colorScheme="blue">Envie um e-mail para a Fazenda</Button>
+        <HStack spacing="4">
+          <Link href="https://www.instagram.com/fazendaitaborai/">
+            <Icon as={RiInstagramLine} fontSize="28"/>
+          </Link>
+          <Link href="https://www.facebook.com/fazendaitaborai">
+            <Icon as={RiFacebookLine} fontSize="28"/>
+          </Link>
+        </HStack>
       </Stack>
     </>
   )
