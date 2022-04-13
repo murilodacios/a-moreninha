58586630645ed144137d8f4c0c05719e6e96235d
Murilo Santos <murilo.santos@itaborai.rj.gov.br>
2022-04-13 10:35:49 -0300

    Chat com problemas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import {
-  Button, HStack, Icon, Image, Link, SimpleGrid, Stack, Text
+  Box,
+  Button, Flex, HStack, Icon, Image, Link, SimpleGrid, Stack, Text
 } from '@chakra-ui/react'
 
 import Head from 'next/head'
@@ -16,6 +17,12 @@ export default function Home() {
       <Head>
         <title>A Moreninha - Nota Fiscal - Secretaria de Fazenda de Itaboraí</title>
       </Head>
+
+      <Flex bg="yellow.300" align="center" justify="center" p="4">
+        <Text>Nosso chat de atendimento neste site está com problemas técnicos, estamos trabalhando para retomar
+          o mais rápido possível.
+        </Text>
+      </Flex>
       
       <Hero />
       
