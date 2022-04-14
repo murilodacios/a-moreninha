87057f8354ece02500f30e820f3da479f78a61e8
Murilo Santos <murilo.santos@itaborai.rj.gov.br>
2022-04-14 08:36:56 -0300

    Add new chat

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,8 @@ export default class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
-                    <script src="//code-eu1.jivosite.com/widget/5IKQWv7EWL" async></script>
+                 
+                    <script id="ze-snippet"src="https://static.zdassets.com/ekr/snippet.js?key=068bb975-eb20-4579-854d-32332f289436"></script>
 
                 </Head>
                 <body>
