c034c58e2eab040382ff0744a16a69a41db2b8c2
Murilo Dácio <murilodacio@gmail.com>
2022-05-23 08:49:25 -0300

    add new jivoo

---
+++ src/components/Items.tsx
@@ -0,0 +1,26 @@
+import { Box, Stack } from "@chakra-ui/react";
+
+export function Items() {
+    return (
+        <>
+
+            <Box
+                maxWidth={1280}
+                m="0 auto"
+                px="4"
+                mt="8"
+                id="servicos"
+                fontFamily="Archivo"
+                mb="24"
+            >
+                <Stack
+                    maxWidth={800}
+                    spacing="4"
+                >
+
+                </Stack>
+            </Box>
+
+        </>
+    )
+}
